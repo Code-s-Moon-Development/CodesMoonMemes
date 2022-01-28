c01e2add24272b8b0688ac7ba5d482ca4be43cea
Rortan <samaritanojr006@gmail.com>
2022-01-28 18:21:50 +0100

    build: set up supabase api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
     <React.StrictMode>
-    <HashRouter>
-        <App />
-    </HashRouter>
+        <HashRouter>
+            <App />
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById("root")
 );
